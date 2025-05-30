2465f6298b96887ea6dbfd82689604f2b0f412e4
Gaganharikiranmarella <ghk7125@gmail.com>
2025-05-30 20:27:20 +0530

    modified q13

--- q13.js
+++ q13.js
@@ -20,4 +20,28 @@ db.orders.insertMany([
   }
 ]);
 //assgn2
-db.orders.find({});
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "employees",       // The collection to join
+      localField: "empid",     // Field from the orders collection
+      foreignField: "_id",     // Field from the employees collection
+      as: "employeeDetails"    // Name of the new array field to add
+    }
+  },
+  {
+    $unwind: "$employeeDetails" // Deconstructs the array to output a document for each element
+  },
+  {
+    $project: {
+      _id: 0,
+      empid: 1,
+      items: 1,
+      orderValue: 1,
+      status: 1,
+      "employeeDetails.name": 1,
+      "employeeDetails.email": 1
+    }
+  }
+]);
+
